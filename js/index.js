29c3eddd2ea367d839388298c163b11d16fe63be
andresaguiarok <andresaguiarok@outlook.com>
2023-07-23 17:38:46 -0300

    my portfolio

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
 console.log('hola estamos bien')
 
 const technologys = document.getElementById("languages")
-const div  = document.createElement("div")
 
-const get = () => {
+function get() {
     fetch("../techLanguages.json")
-    .then(res => res.json())
-    .then(res => res.forEach(item => {
-        technologys.innerHTML += `
+        .then(res => res.json())
+        .then(res => res.forEach(item => {
+            technologys.innerHTML += `
         <img class="col-3 animate__animated animate__backInLeft" src=${item.icon} >
         `
-    }))
-    .catch(error => console.log(error))
+        }))
+        .catch(error => console.log(error))
 }
 
 get()
