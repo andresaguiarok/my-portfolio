ffe1340c44f4d0c3a9bc3855064292e7f127df57
andresaguiarok <andresaguiarok@outlook.com>
2023-08-10 19:05:18 -0300

    prueba

--- js/index.js
+++ js/index.js
@@ -1,22 +1,14 @@
-console.log('hola estamos bien')
-
 const technologys = document.getElementById("languages")
 
 function get() {
     fetch("../techLanguages.json")
         .then(res => res.json())
-        .then(res => res.forEach(item => {
+        .then(res => res.map(item => {
             technologys.innerHTML += `
-        <img class="col-3 animate__animated animate__backInLeft" src=${item.icon} >
+        <img class="col-3 animate__animated animate__backInLeft animate__slower" src=${item.icon} >
         `
         }))
         .catch(error => console.log(error))
 }
 
 get()
-
-
-
-
-
-
