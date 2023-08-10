f0788c5c3e80796895065695a68f1b08d395ba9c
andresaguiarok <andresaguiarok@outlook.com>
2023-08-10 20:14:36 -0300

    tecnologias frontend

--- js/index.js
+++ js/index.js
@@ -4,17 +4,9 @@ const languages = []
 function get() {
     fetch("./techLanguages.json")
         .then(res => res.json())
-        .then(res => res.map(item => languages.push(item) ))
+        .then(res => res.map(item => technologys.innerHTML += `
+        <img class="col-3 animate__animated animate__backInLeft animate__slower" src=${item.icon} >
+        `))
         .catch(error => console.log(error))
 }
 get()
-
-setTimeout(() => {
-    let div = document.createElement ("div");
-    languages.forEach(item => {
-        div.innerHTML += `
-        <img class="col animate__animated animate__backInLeft animate__slower" src=${item.icon} >
-        `
-        technologys.append(div)
-    })
-  }, 1000);
